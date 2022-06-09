fa381435bd40aa0de08de5f0289136713d2bdadd
Feller <iamliurenjie@gmail.com>
2022-06-09 16:32:44 +0800

    Update freq.js

--- freq.js
+++ freq.js
@@ -31,11 +31,11 @@ if (inputValue) {
             "Content-Type": "application/json",
             "Authentication": "35361c848ad548b2954c60e711b7aff9",
             "Token": "13a589cd26c6980b7a3b76555f4d9140368603f31b34b202cbfbf8b9415fffc1beae85a13b800808785042a62ceceb340634a671c5209c3fb96176f62681e004df598d5bfd7a676c3939aca7287f8c72691fa45f6fff5a7dff285f0f88fc77f7bd8b8209832ff82a24e30dff41c4ebd3e86ebd110c0cd0fec5a327d24523a2bb",
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.23(0x18001725) NetType/4G Language/zh_CN",
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001628) NetType/WIFI Language/zh_CN",
             "Referer": "https://servicewechat.com/wxc5059c3803665d9c/456/page-frame.html",
             "Accept-Encoding": "gzip",
         },
-        body: {"params":{},"userSystemData":{"brand":"iPhone","model":"iPhone 12 Pro Max<iPhone13,2>","version":"8.0.23","system":"iOS 15.5","platform":"ios","SDKVersion":"2.24.5","windowWidth":428},"sessionId":"35361c848ad548b2954c60e711b7aff9"}
+        body: {"params":{},"userSystemData":{"brand":"iPhone","model":"iPhone 12 Pro Max<iPhone13,4>","version":"8.0.22","system":"iOS 16.0","platform":"ios","SDKVersion":"2.24.5","windowWidth":428},"sessionId":"35361c848ad548b2954c60e711b7aff9"}
       });
 
     let mw = resp_mw.data.data
@@ -46,13 +46,13 @@ if (inputValue) {
         header: {
             //这里需要替换自己的
             'Host': 'suishenmaback2.sh.gov.cn',
-			'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundarymr9WYoAKxVtQ2F7C',
+			'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryLYZBL1SQiWEH4HiB',
 			'Origin': 'https://suishenmaback2.sh.gov.cn',
 			'Accept-Encoding': 'gzip, deflate, br',
 			'Connection': 'keep-alive',
 			'Accept': 'application/json, text/plain, */*',
-			'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001628) NetType/WIFI Language/zh_CN miniProgram/wxc5059c3803665d9c',
-			'Referer': 'https://suishenmaback2.sh.gov.cn/smzy/h5/preventionV2?from=wx&mw=MSpMNMFzg3OiT96RONyNI7Lcti8tl7nb6saysLjzEzRlBtrxdx0rhXc%2Flrij5vaSnfmxBU31T3XsK54c6BNflqfeTlzzTTSSpxxhqGXYBtlYdywylyr%2FIMWlDiARmuXJnhpWrCNWRkr3rlycob5EdQ%3D%3D&latitude=31.223839684896895&longitude=121.5260533190952&_t=1654758335042',
+			'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001628) NetType/WIFI Language/zh_CN',
+			'Referer': 'https://suishenmaback2.sh.gov.cn/smzy/h5/preventionV2?from=wx&mw=MSpMNMFzg3OiT96RONyNI7Lcti8tl7nb6saysLjzEzRlBtrxdx0rhXc%2Flrij5vaSnfmxBU31T3XsK54c6BNflquMrvb%2Fsx2GQDzxDWwpLys1rp%2F%20CaLpiao70kYJlfhro%2FxXQeZyyQfKCu4KVXdWYw%3D%3D&latitude=31.22390126575324&longitude=121.52603880715792&_t=1654763159747',
 			'Content-Length': '293',
 			'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
         },
@@ -69,14 +69,14 @@ if (inputValue) {
         header: {
             //这里需要替换自己的
             'Host': 'suishenmaback2.sh.gov.cn',
-			'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryKDs7ygMa7Aa0u9hG',
+			'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryIHmc7eeacCQul3df',
 			'Origin': 'https://suishenmaback2.sh.gov.cn',
 			'Accept-Encoding': 'gzip, deflate, br',
-			'Cookie': 'WT-group10-3=ac1e7d2e300c0c790050; WT-group10=CFDQKwjgEqwU9Rocb7A/GA$$',
+			'Cookie': 'WT-group10-3=ac1e7d2c8bf9122c0050; WT-group10=BnOQDgjgEqzoakQWA75mVQ$$',
 			'Connection': 'keep-alive',
 			'Accept': 'application/json, text/plain, */*',
-			'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001628) NetType/WIFI Language/zh_CN miniProgram/wxc5059c3803665d9c',
-			'Referer': 'https://suishenmaback2.sh.gov.cn/smzy/h5/preventionV2?from=wx&mw=MSpMNMFzg3OiT96RONyNI7Lcti8tl7nb6saysLjzEzRlBtrxdx0rhXc%2Flrij5vaSnfmxBU31T3XsK54c6BNflssZZQBeot8r6NOXtpK0RjyWTX8XywVxyuEocUSx23b8%20u%209Hb1%20MQe7Pa5DSjtoog%3D%3D&latitude=31.223839684896895&longitude=121.5260533190952&_t=1654759128810',
+			'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001628) NetType/WIFI Language/zh_CN',
+			'Referer': 'https://suishenmaback2.sh.gov.cn/smzy/h5/preventionV2?from=wx&mw=MSpMNMFzg3OiT96RONyNI7Lcti8tl7nb6saysLjzEzRlBtrxdx0rhXc%2Flrij5vaSnfmxBU31T3XsK54c6BNflquMrvb%2Fsx2GQDzxDWwpLys1rp%2F%20CaLpiao70kYJlfhro%2FxXQeZyyQfKCu4KVXdWYw%3D%3D&latitude=31.22390126575324&longitude=121.52603880715792&_t=1654763159747',
 			'Content-Length': '425',
 			'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
         },
